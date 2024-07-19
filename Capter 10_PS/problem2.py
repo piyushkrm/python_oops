16af23a966f0c1e2bae37f3898c6c94e2aee6197
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-19 11:04:52 +0000

    problem 2 is solved

---
+++ Capter 10_PS/problem2.py
@@ -0,0 +1,21 @@
+'''Write a class "calculator" capable of finding square, cube and square root of a number.'''
+
+class calculator() :
+
+    def __init__(self, number):
+        self.number = number
+
+    def square(self) :
+        print(f"Square of your entered number is : {self.number * self.number}")
+
+    def cube(self) :
+        print(f"Cube of your entered number is : {(self.number * self.number)*self.number}")
+
+    def squareroot(self) :
+        print(f"Squareroot of your entered number is : {self.number ** 1/2}")
+
+
+cal = calculator(3)
+cal.square()
+cal.cube()
+cal.squareroot()
