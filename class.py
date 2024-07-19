966a1f1fb84192876742726e127c2c69416e6bf7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-19 10:09:58 +0000

    create objects of class

---
+++ class.py
@@ -0,0 +1,11 @@
+class employe :
+    language = "python"
+    salary = 1500000
+
+piyush = employe()          #object of employee class
+piyush.name = "piysuh Mishra"
+print(piyush.salary, piyush.name, piyush.language)
+
+aayush = employe()
+aayush.name = "Aayush"
+print(aayush.name, aayush.salary, aayush.language)
