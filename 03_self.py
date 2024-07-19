006180db5c5d04bc047b2cc9bd9193bd86477954
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-19 10:22:31 +0000

    Self parameter

---
+++ 03_self.py
@@ -0,0 +1,8 @@
+class employe :
+    language = "python" 
+    salary = 1500000
+    def getInfo(self) :
+        print(f"The language is {self.language}.\nAnd the salary is {self.salary}")
+
+piyush = employe()         
+piyush.getInfo()
