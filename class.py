8bfb12f1d079b78f9779848db8e8fcc187e00dd8
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-19 10:12:36 +0000

    class and instance(object) attribute

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 class employe :
-    language = "python"
+    language = "python"     #This is the class attribute
     salary = 1500000
 
 piyush = employe()          #object of employee class
@@ -7,5 +7,7 @@ piyush.name = "piysuh Mishra"
 print(piyush.salary, piyush.name, piyush.language)
 
 aayush = employe()
-aayush.name = "Aayush"
+aayush.name = "Aayush"      #This is the instance(object) attribute
 print(aayush.name, aayush.salary, aayush.language)
+
+# Here name is instance(object) attribute and salary and langauge are class attribute as they directly belog to the class
