16037b3d3bd1353ce22d52d4d6ab4fb03a57da3f
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-20 09:12:26 +0530

    Inheritance used

--- Chapter 11/01_inheritance.py
+++ Chapter 11/01_inheritance.py
@@ -4,14 +4,22 @@ class Employee:
         print(f"The name is {self.name} and the salary is {self.salary}")
 
 
-class Programmer :
+# class Programmer :
+#     company = "ITC Infotect"
+#     def show(self) :
+#         print(f"The name is {self.name} and the salary is {self.salary}")
+
+#     def show_language(self) :
+#         print(f"The name is {self.name} and he is good with {self.language} language")
+    
+#  Instant of writing these code we use inheritance and redure line of code and error mistakes
+
+class Programmer(Employee) :            # This is the inheritance
     company = "ITC Infotect"
     def show(self) :
         print(f"The name is {self.name} and the salary is {self.salary}")
 
-    def show_language(self) :
-        print(f"The name is {self.name} and he is good with {self.language} language")
-    
+
 a = Employee()
 b = Programmer()
 
